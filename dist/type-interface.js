067cb2680aac64f918a3e2dbd6a05aa99c9cbdd7
Melik <melikpapikyan@gmail.com>
2026-06-27 00:32:01 +0400

    type-interface

--- dist/type-interface.js
+++ dist/type-interface.js
@@ -18,4 +18,48 @@ const student = [
 student.forEach((item) => {
     console.log(item.name);
 });
+const products = [
+    {
+        id: 1,
+        title: 'Apple',
+        price: 5
+    },
+    {
+        id: 2,
+        title: 'Bread',
+        price: 1.9
+    },
+    {
+        id: 3,
+        title: 'Cheese',
+        price: 15
+    },
+    {
+        id: 4,
+        title: 'Egg',
+        price: 5
+    },
+    {
+        id: 5,
+        title: 'Rice',
+        price: 3
+    }
+];
+const employees = [
+    {
+        name: 'Melik',
+        age: 40,
+        position: 'Front-End developer',
+        salary: 5000
+    },
+    {
+        name: 'James',
+        age: 22,
+        position: 'Full-Stack developer',
+        salary: 7500
+    }
+];
+employees.forEach((item) => {
+    console.log(item.position);
+});
 export {};
--- src/type-interface.ts
+++ src/type-interface.ts
@@ -26,4 +26,77 @@ const student: Students[] = [
 student.forEach((item) => {
     console.log(item.name);
 })
+//დავალება 2 — Product Interface
+interface Product {
+    id: number;
+    title: string;
+    price: number;
+}
 
+const products: Product[] = [
+    {
+        id: 1,
+        title: 'Apple',
+        price: 5
+
+    },
+    {
+        id: 2,
+        title: 'Bread',
+        price: 1.9
+
+    },
+    {
+        id: 3,
+        title: 'Cheese',
+        price: 15
+
+    },
+    {
+        id: 4,
+        title: 'Egg',
+        price: 5
+
+    },
+    {
+        id: 5,
+        title: 'Rice',
+        price: 3
+
+    }
+]
+
+
+
+
+//დავალება 3 — Extends-ის გამოყენება
+interface Person {
+    name: string;
+    age: number;
+}
+
+
+interface Employee extends Person {
+    position: string;
+    salary: number;
+}
+
+const employees: Employee[] = [
+    {
+        name: 'Melik',
+        age: 40,
+        position: 'Front-End developer',
+        salary: 5000
+    },
+    {
+        name: 'James',
+        age: 22,
+        position: 'Full-Stack developer',
+        salary: 7500
+    }
+
+]
+
+employees.forEach((item) => {
+    console.log(item.position);
+})
