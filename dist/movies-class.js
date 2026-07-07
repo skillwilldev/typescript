ce47bedc0b0ea38a3603ebcfaac5771f2048d688
Melik <melikpapikyan@gmail.com>
2026-07-07 18:07:00 +0400

    movies-class

--- dist/movies-class.js
+++ dist/movies-class.js
@@ -41,16 +41,16 @@ class Movie {
     }
 }
 const movies = [
-    new Movie(1, "Forrest Gump", "Robert Zemeckis", "drama", 1994, 8.8, "Watched"),
-    new Movie(2, "The Shawshank Redemption", "Frank Darabont", "drama", 1994, 9.3, "Watched"),
-    new Movie(3, "Inception", "Christopher Nolan", "sci-fi", 2010, 8.8, "Watched"),
-    new Movie(4, "The Godfather", "Francis Ford Coppola", "crime", 1972, 9.2, "Want To Watch"),
-    new Movie(5, "Pulp Fiction", "Quentin Tarantino", "crime", 1994, 8.9, "Watched"),
-    new Movie(6, "Interstellar", "Christopher Nolan", "sci-fi", 2014, 8.6, "Want To Watch"),
-    new Movie(7, "The Dark Knight", "Christopher Nolan", "action", 2008, 9.0, "Watched"),
-    new Movie(8, "Fight Club", "David Fincher", "drama", 1999, 8.8, "Didn't Watch"),
-    new Movie(9, "The Matrix", "Lana Wachowski", "sci-fi", 1999, 8.7, "Watched"),
-    new Movie(10, "Parasite", "Bong Joon-ho", "thriller", 2019, 8.6, "Want To Watch"),
+// new Movie(1, "Forrest Gump", "Robert Zemeckis", "drama", 1994, 8.8, "Watched"),
+// new Movie(2, "The Shawshank Redemption", "Frank Darabont", "drama", 1994, 9.3, "Watched"),
+// new Movie(3, "Inception", "Christopher Nolan", "sci-fi", 2010, 8.8, "Watched"),
+// new Movie(4, "The Godfather", "Francis Ford Coppola", "crime", 1972, 9.2, "Want To Watch"),
+// new Movie(5, "Pulp Fiction", "Quentin Tarantino", "crime", 1994, 8.9, "Watched"),
+// new Movie(6, "Interstellar", "Christopher Nolan", "sci-fi", 2014, 8.6, "Want To Watch"),
+// new Movie(7, "The Dark Knight", "Christopher Nolan", "action", 2008, 9.0, "Watched"),
+// new Movie(8, "Fight Club", "David Fincher", "drama", 1999, 8.8, "Didn't Watch"),
+// new Movie(9, "The Matrix", "Lana Wachowski", "sci-fi", 1999, 8.7, "Watched"),
+// new Movie(10, "Parasite", "Bong Joon-ho", "thriller", 2019, 8.6, "Want To Watch"),
 ];
 movies[0]?.getMovieTitle();
 class MovieStaff {
@@ -150,4 +150,59 @@ class Tester extends MovieStaff {
         return this.index;
     }
 }
-export {};
+const myForm = document.getElementById("myForm");
+const form = document.createElement("form");
+const titleInput = document.createElement("input");
+titleInput.type = "text";
+titleInput.placeholder = "title";
+const directorInput = document.createElement("input");
+directorInput.type = "text";
+directorInput.placeholder = "director";
+const genreInput = document.createElement("input");
+genreInput.type = "text";
+genreInput.placeholder = "genre";
+const yearInput = document.createElement("input");
+yearInput.type = "text";
+yearInput.placeholder = "year";
+const ratingInput = document.createElement("input");
+ratingInput.type = "number";
+ratingInput.placeholder = "Rating";
+const statusInput = document.createElement("select");
+const neutral = document.createElement("option");
+neutral.value = "";
+neutral.textContent = "Choose Status";
+const watchedSelect = document.createElement("option");
+watchedSelect.value = "Watched";
+watchedSelect.textContent = "Watched";
+const notwatchedSelect = document.createElement("option");
+notwatchedSelect.value = "Didn't Watch";
+notwatchedSelect.textContent = "Didn't Watch";
+const button = document.createElement("button");
+button.type = "submit";
+button.textContent = "OK";
+statusInput.append(neutral, watchedSelect, notwatchedSelect);
+form.append(titleInput, directorInput, genreInput, yearInput, ratingInput, statusInput, button);
+myForm.appendChild(form);
+function loadData() {
+    let data = localStorage.getItem("films");
+    if (data) {
+        movies.push(JSON.parse(data));
+    }
+}
+loadData();
+function saveData() {
+    localStorage.setItem("films", JSON.stringify(movies));
+}
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let title = titleInput.value.trim();
+    let director = directorInput.value.trim();
+    let genre = genreInput.value.trim();
+    let year = Number(yearInput.value.trim());
+    let rating = Number(ratingInput.value.trim());
+    let state = statusInput.value;
+    let newMovie = new Movie(Date.now(), title, director, genre, year, rating, state === "Watched" ? "Watched" : "Didn't Watch");
+    movies.push(newMovie);
+    saveData();
+});
+
